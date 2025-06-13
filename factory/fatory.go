33496660c4e59f7e4c10ed67df7f3b5857bbe2d5
995933447 <34621597+995933447@users.noreply.github.com>
2025-06-13 18:32:22 +0400

    Update fatory.go

--- factory/fatory.go
+++ factory/fatory.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"github.com/995933447/autoelectv2"
 	"github.com/995933447/autoelectv2/impl/distribmu/etcdv3"
+	rediselect "github.com/995933447/autoelectv2/impl/distribmu/redis"
+	"github.com/gomodule/redigo/redis"
 	clientv3 "go.etcd.io/etcd/client/v3"
 )
 
@@ -12,6 +14,7 @@ type ElectDriver int
 const (
 	ElectDriverNil = iota
 	ElectDriverDistribMuEtcdv3
+	ElectDriverDistribMuRedis
 )
 
 type DistribMuEtcdv3Cfg struct {
@@ -28,11 +31,28 @@ func NewDistribMuEtcdv3Cfg(cluster string, etcdCli *clientv3.Client, masterTenan
 	}
 }
 
+type DistribMuRedisCfg struct {
+	redisPool *redis.Pool
+	cluster   string
+	nodeId    string
+}
+
+func NewDistribMuRedisCfg(redisPool *redis.Pool, cluster string, nodeId string) *DistribMuRedisCfg {
+	return &DistribMuRedisCfg{
+		redisPool: redisPool,
+		cluster:   cluster,
+		nodeId:    nodeId,
+	}
+}
+
 func NewAutoElection(driver ElectDriver, cfg any) (autoelectv2.AutoElection, error) {
 	switch driver {
 	case ElectDriverDistribMuEtcdv3:
 		specCfg := cfg.(*DistribMuEtcdv3Cfg)
 		return etcdv3.New(specCfg.cluster, specCfg.etcdCli, specCfg.masterTenancySec)
+	case ElectDriverDistribMuRedis:
+		specCfg := cfg.(*DistribMuRedisCfg)
+		return rediselect.NewRedisElect(specCfg.cluster, specCfg.nodeId, specCfg.redisPool)
 	}
 	return nil, fmt.Errorf("invalid driver:%d", driver)
 }
